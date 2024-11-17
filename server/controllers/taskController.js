e0995e23c0878b80d320aa75cbbe3473ccb0e961
nexifyyy <nexify045@gmail.com>
2024-11-17 22:52:10 +0530

    ENV ISSUE

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -606,18 +606,46 @@ export const trashTask = async (req, res) => {
   }
 };
 
+// export const deleteRestoreTask = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const { isRestore } = req.body;
+//     const task = await Task.findById(id);
+//     task.isTrashed = true;
+//     await task.save();
+//     res.status(200).json({
+//       status: true,
+//       message: isRestore
+//         ? `Task restored successfully.`
+//         : `Task deleted successfully.`,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(400).json({ status: false, message: error.message });
+//   }
+// };
+
 export const deleteRestoreTask = async (req, res) => {
   try {
     const { id } = req.params;
-    const { isRestore } = req.body;
-    const task = await Task.findById(id);
-    task.isTrashed = !isRestore;
-    await task.save();
+    const { actionType } = req.query;
+    if (actionType === "delete") {
+      await Task.findByIdAndDelete(id);
+    } else if (actionType === "deleteAll") {
+      await Task.deleteMany({ isTrashed: true });
+    } else if (actionType === "restore") {
+      const resp = await Task.findById(id);
+      resp.isTrashed = false;
+      resp.save();
+    } else if (actionType === "restoreAll") {
+      await Task.updateMany(
+        { isTrashed: true },
+        { $set: { isTrashed: false } }
+      );
+    }
     res.status(200).json({
       status: true,
-      message: isRestore
-        ? `Task restored successfully.`
-        : `Task deleted successfully.`,
+      message: `Operation performed successfully.`,
     });
   } catch (error) {
     console.log(error);
