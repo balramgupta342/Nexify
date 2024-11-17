591c4a117d73f7ab58a374d5d28b02c128afe394
GaganTomarr <98637342+GaganTomarr@users.noreply.github.com>
2024-11-18 01:55:56 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
 
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
     proxy: {
       "/api": {
         target: "http://localhost:8800",
