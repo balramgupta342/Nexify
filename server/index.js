9e7c29a67816efc3a932fe7cd50e25519630e8c1
GaganTomarr <98637342+GaganTomarr@users.noreply.github.com>
2024-11-18 02:40:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,8 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+const cors = require('cors');
+
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
